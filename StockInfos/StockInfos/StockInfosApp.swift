987202f373b709a09021747a08f59528157f4499
박희태 <heetae.park@yanolja.com>
2022-11-09 23:19:17 +0900

    프로젝트 생성

---
+++ StockInfos/StockInfos/StockInfosApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockInfosApp.swift
+//  StockInfos
+//
+//  Created by 희태 박 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct StockInfosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
